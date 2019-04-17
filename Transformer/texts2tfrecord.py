2d009fcf6cc464d629bbb006928220188ce5af77
shillyshallysxy <895484721@qq.com>
2019-04-18 00:17:30 +0800

    第一版transformer

---
+++ Transformer/texts2tfrecord.py
@@ -0,0 +1,139 @@
+from Encoder_Decoder import text_helper
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.framework import ops
+from nltk.corpus import stopwords
+from tqdm import tqdm
+import pickle
+import os
+ops.reset_default_graph()
+
+sess = tf.Session()
+create_tfrecord = True
+data_folder_name = '..\\temp'
+data_path_name = 'cn_nlp\\translate'
+vocab_name_cn = 'translate_cn.pkl'
+vocab_name_en = 'translate_en.pkl'
+train_record_name = 'translate_cn_en_train.tfrecord'
+test_record_name = 'translate_cn_en_test.tfrecord'
+
+vocab_path_cn = os.path.join(data_folder_name, data_path_name, vocab_name_cn)
+vocab_path_en = os.path.join(data_folder_name, data_path_name, vocab_name_en)
+
+if create_tfrecord:
+    # load movie review data
+    en_data, en_len, cn_data, cn_len = text_helper.load_data()
+    test_en_data, test_en_len, test_cn_data, test_cn_len = text_helper.load_test_data()
+
+    if os.path.isfile(vocab_path_cn):
+        with open(vocab_path_cn, 'rb') as f:
+            word_dict_cn = pickle.load(f)
+    else:
+        print('creating dictionary')
+        word_dict_cn = text_helper.build_dictionary(cn_data, 'cn')
+        with open(vocab_path_cn, 'wb') as f:
+            pickle.dump(word_dict_cn, f)
+
+    if os.path.isfile(vocab_path_en):
+        with open(vocab_path_en, 'rb') as f:
+            word_dict_en = pickle.load(f)
+    else:
+        print('creating dictionary')
+        word_dict_en = text_helper.build_dictionary(en_data, 'en')
+        with open(vocab_path_en, 'wb') as f:
+            pickle.dump(word_dict_en, f)
+
+    vocabulary_size_cn = len(word_dict_cn)
+    vocabulary_size_en = len(word_dict_en)
+    en_max_len = max(en_len)
+    cn_max_len = max(cn_len)
+    print(vocabulary_size_cn, vocabulary_size_en)
+    print(cn_max_len, en_max_len)
+    # exit()
+    cn_data_num = text_helper.text_to_numbers(cn_data, word_dict_cn, cn_len, 'cn')
+    test_cn_data_num = text_helper.text_to_numbers(test_cn_data, word_dict_cn, test_cn_len, 'cn')
+    en_data_num = text_helper.text_to_numbers(en_data, word_dict_en, en_len)
+    test_en_data_num = text_helper.text_to_numbers(test_en_data, word_dict_en, test_en_len)
+else:
+    with open(vocab_path_en, 'rb') as f:
+        word_dict_en = pickle.load(f)
+    with open(vocab_path_cn, 'rb') as f:
+        word_dict_cn = pickle.load(f)
+    vocabulary_size_cn = len(word_dict_cn)
+    vocabulary_size_en = len(word_dict_en)
+
+
+def write_binary(record_name, texts_, target_, text_lens_, target_lens_):
+    record_path = os.path.join(data_folder_name, data_path_name, record_name)
+    writer = tf.python_io.TFRecordWriter(record_path)
+    for it, text in tqdm(enumerate(texts_)):
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    "text": tf.train.Feature(int64_list=tf.train.Int64List(value=text)),
+                    "label": tf.train.Feature(int64_list=tf.train.Int64List(value=target_[it]+[word_dict_en['_EOS']])),
+                    "text_length": tf.train.Feature(int64_list=tf.train.Int64List(value=[text_lens_[it]])),
+                    "label_length": tf.train.Feature(int64_list=tf.train.Int64List(value=[target_lens_[it]+1]))
+                }
+            )
+        )
+        serialized = example.SerializeToString()
+        writer.write(serialized)
+    writer.close()
+
+
+def __parse_function(serial_exmp):
+    features = tf.parse_single_example(serial_exmp, features={"text": tf.VarLenFeature(tf.int64),
+                                                              "label": tf.VarLenFeature(tf.int64),
+                                                              "text_length": tf.FixedLenFeature([], tf.int64),
+                                                              "label_length": tf.FixedLenFeature([], tf.int64)})
+    # text = tf.sparse_tensor_to_dense(features["text"], default_value=" ")
+    text_ = tf.sparse_tensor_to_dense(features["text"])
+    label_ = tf.sparse_tensor_to_dense(features["label"])
+    text_lens_ = tf.cast(features["text_length"], tf.int32)
+    label_lens_ = tf.cast(features["label_length"], tf.int32)
+    return text_, label_, text_lens_, label_lens_
+
+
+write_binary(train_record_name, cn_data_num, en_data_num, cn_len, en_len)
+write_binary(test_record_name, test_cn_data_num, test_en_data_num, test_cn_len, test_en_len)
+# exit()
+record_path = os.path.join(data_folder_name, data_path_name, train_record_name)
+dataset = tf.data.TFRecordDataset(record_path)
+dataset = dataset.map(__parse_function)
+data_train = dataset.shuffle(1000).repeat(10).padded_batch(1, padded_shapes=([None], [None], [], []))
+iter_train = data_train.make_one_shot_iterator()
+# text_data, label_data = iter_train.get_next()
+# with tf.Session() as sess:
+#     for i in range(10):
+#         print(sess.run([text_data, label_data]))
+#         print(sess.run(tf.shape(text_data)))
+#         # print(sess.run(iterator))
+# print('')
+
+
+handle = tf.placeholder(tf.string, shape=[])
+iterator = tf.data.Iterator.from_string_handle(handle, data_train.output_types, data_train.output_shapes)
+x, y_, x_l_, y_l_ = iterator.get_next()
+
+# table_name = 'text_table.txt'
+# with open(table_name, 'w') as f:
+#     for word in word_dict.values():
+#         f.write(str(word)+'\n')
+# text_lookup_table = tf.contrib.lookup.index_table_from_file(vocabulary_file=table_name,
+#                                                             num_oov_buckets=1)
+# embed = tf.nn.embedding_lookup(embeddings, x)
+# ids = text_lookup_table.lookup()
+# sess.run(tf.tables_initializer())
+handle_train = sess.run(iter_train.string_handle())
+# print(handle_train)
+# print(sess.run(ids, feed_dict={handle: handle_train}))
+for i in range(1000):
+    a, b, c, d = sess.run([x, y_, x_l_, y_l_], feed_dict={handle: handle_train})
+    # print(a, b, c, d)
+    if len(b[0]) != d[0] or len(a[0]) != c[0]:
+        print(text_helper.numbers_to_text(b, word_dict_en), d[0])
+        print(text_helper.numbers_to_text(a, word_dict_cn), c[0])
+
+# print(sess.run(x_split, feed_dict={handle: handle_train}))
+
---
+++ Transformer/transformer_utils.py
@@ -0,0 +1,240 @@
+import tensorflow as tf
+import math
+import numpy as np
+
+
+def gelu(inputs):
+    """
+    gelu: https://arxiv.org/abs/1606.08415
+    :param inputs: [Tensor]
+    :return: [Tensor] outputs after activation
+    """
+    cdf = 0.5 * (1.0 + tf.tanh(tf.sqrt(2 / np.pi) * (inputs + 0.044715 * tf.pow(inputs, 3))))
+    return inputs * cdf
+
+
+def get_activation(activation_name):
+    """
+    get activate function
+    :param activation_name: [Tensor]
+    :return: [Function] activation function
+    """
+    if activation_name is None:
+        return gelu
+    else:
+        act = activation_name.lower()
+        if act == "relu":
+            return tf.nn.relu
+        elif act == "gelu":
+            return gelu
+        elif act == "tanh":
+            return tf.tanh
+        else:
+            raise ValueError("Unsupported activation: %s" % act)
+
+
+def reshape_to_matrix(inputs):
+    """
+    Reshapes inputs tensor to a rank 2 tensor.
+    :param inputs: [Tensor]
+    :return: [Tensor] tensor with two dimensions
+    """
+    ndims = inputs.shape.ndims
+    if ndims < 2:
+        raise ValueError("Input tensor must have at least rank 2. Shape = {}".format(inputs.shape))
+    if ndims == 2:
+        return inputs
+
+    width = inputs.shape[-1]
+    output_tensor = tf.reshape(inputs, [-1, width])
+    return output_tensor
+
+
+def group_norm(inputs: tf.Tensor, epsilon=1e-8, scope="layer_normalization", reuse=None):
+    """
+    layer normalization
+    :param inputs: [Tensor] with first dimension of "batch_size"
+    :param epsilon: [Float] a number for preventing ZeroDivision
+    :param scope: [String] name of "variable_scope"
+    :param reuse: [Boolean] tf parameter reuse
+    :return: [Tensor] outputs after normalized
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        inputs_shape = inputs.get_shape()
+        params_shape = inputs_shape[-1:]
+        mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+        beta = tf.Variable(tf.zeros(params_shape))
+        gamma = tf.Variable(tf.ones(params_shape))
+        normalized = (inputs - mean) * tf.rsqrt(variance + epsilon)
+        outputs = gamma * normalized + beta
+    return outputs
+
+
+def get_positional_encoding(inputs, channels, scope="positional_embedding", reuse=None):
+    """
+    positional encoding
+    :param inputs: [Tensor] with dimension of "batch_size * max_length"
+    :param channels: [Int] Embedding size
+    :param scope: [String] name of "variable_scope"
+    :param reuse: [Boolean] tf parameter reuse
+    :return: [Tensor] outputs after positional encoding
+    """
+    N, T = inputs.get_shape().as_list()
+
+    with tf.variable_scope(scope, reuse=reuse):
+        position_signal = get_timing_signal_1d(T, channels)
+        position_signal = tf.tile(position_signal, [N, 1, 1])
+    return position_signal
+
+
+def get_timing_signal_1d(length, channels, min_timescale=1.0, max_timescale=1.0e4, start_index=0):
+    """
+    positional encoding的方法
+    :param length: [Int] max_length size
+    :param channels: [Int] Embedding size
+    :param min_timescale: [Float]
+    :param max_timescale: [Float]
+    :param start_index: [Int] index of first position
+    :return: [Tensor] positional encoding of shape "1 * length * channels"
+    """
+    position = tf.to_float(tf.range(length) + start_index)
+    num_timescales = channels // 2
+    log_timescale_increment = (math.log(float(min_timescale) / float(max_timescale)) /
+                               (tf.to_float(num_timescales) - 1))
+    inv_timescales = min_timescale * tf.exp(tf.to_float(tf.range(num_timescales)) * -log_timescale_increment)
+
+    scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
+    signal = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
+    signal = tf.pad(signal, [[0, 0], [0, tf.mod(channels, 2)]])
+    signal = tf.reshape(signal, [1, length, channels])
+    return signal
+
+
+def get_embedding(inputs, vocab_size, channels, scale=True, scope="embedding", reuse=None):
+    """
+    embedding
+    :param inputs: [Tensor] with first dimension of "batch_size"
+    :param vocab_size: [Int] Vocabulary size
+    :param channels: [Int] Embedding size
+    :param scale: [Boolean] If True, the output will be multiplied by sqrt num_units
+    :param scope: [String] name of "variable_scope"
+    :param reuse: [Boolean] tf parameter reuse
+    :return: [Tensor] outputs of embedding of sentence with shape of "batch_size * length * channels"
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        lookup_table = tf.get_variable('lookup_table',
+                                       dtype=tf.float32,
+                                       shape=[vocab_size, channels],
+                                       initializer=tf.contrib.layers.xavier_initializer())
+
+        outputs = tf.nn.embedding_lookup(lookup_table, inputs)
+
+        if scale:
+            outputs = outputs * math.sqrt(channels)
+
+    return outputs
+
+
+def multi_head_attention(from_tensor: tf.Tensor,  to_tensor: tf.Tensor, channels=None, num_heads=8, dropout_rate=0,
+                         is_training=True, causality=False, scope="multihead_attention", activation=None, reuse=None):
+    """
+    multihead attention
+    :param from_tensor: [Tensor] query matrix
+    :param to_tensor: [Tensor] key matrix
+    :param channels: [Int] channel size of matrix Q, K, V
+    :param num_heads: [Int] head number of attention
+    :param dropout_rate: [Float] dropout rate when 0 means no dropout
+    :param is_training: [Boolean] whether it is training, If true, use dropout
+    :param causality: [Boolean] If true, units that reference the future are masked
+    :param scope: [String] name of "variable_scope"
+    :param activation: [String] name of activate function
+    :param reuse: [Boolean] tf parameter reuse
+    :return: [Tensor] outputs after multihead self attention with shape of "batch_size * max_length * (channels*num_heads)"
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        if channels is None:
+            channels = from_tensor.get_shape().as_list()[-1]
+        activation_fn = get_activation(activation)
+        # shape [batch_size, max_length, channels*num_heads]
+        query_layer = tf.layers.dense(from_tensor, channels * num_heads, activation=activation_fn)
+        key_layer = tf.layers.dense(to_tensor, channels * num_heads, activation=activation_fn)
+        value_layer = tf.layers.dense(to_tensor, channels * num_heads, activation=activation_fn)
+
+        # shape [batch_size*num_heads, max_length, channels]
+        query_layer_ = tf.concat(tf.split(query_layer, num_heads, axis=2), axis=0)
+        key_layer_ = tf.concat(tf.split(key_layer, num_heads, axis=2), axis=0)
+        value_layer_ = tf.concat(tf.split(value_layer, num_heads, axis=2), axis=0)
+
+        # shape = [batch_size*num_heads, max_length, max_length]
+        attention_scores = tf.matmul(query_layer_, tf.transpose(key_layer_, [0, 2, 1]))
+        # Scale
+        attention_scores = tf.multiply(attention_scores, 1.0 / tf.sqrt(float(channels)))
+        # attention masks
+        attention_masks = tf.sign(tf.reduce_sum(to_tensor, axis=-1))
+        attention_masks = tf.tile(attention_masks, [num_heads, 1])
+        attention_masks = tf.tile(tf.expand_dims(attention_masks, axis=1), [1, channels, 1])
+        neg_inf_matrix = tf.multiply(tf.ones_like(attention_scores), (-math.pow(2, 32) + 1))
+        attention_scores = tf.where(tf.equal(attention_masks, 0), neg_inf_matrix, attention_scores)
+
+        if causality:
+            diag_vals = tf.ones_like(attention_scores[0, :, :])
+            tril = tf.contrib.linalg.LinearOperatorTriL(diag_vals).to_dense()
+            masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(attention_scores)[0], 1, 1])
+            neg_inf_matrix = tf.multiply(tf.ones_like(masks), (-math.pow(2, 32) + 1))
+            attention_scores = tf.where(tf.equal(masks, 0), neg_inf_matrix, attention_scores)
+
+        # attention probability
+        attention_probs = tf.nn.softmax(attention_scores)
+
+        # query mask
+        query_masks = tf.sign(tf.abs(tf.reduce_sum(from_tensor, axis=-1)))
+        # shape = [N*h, T_q]
+        query_masks = tf.tile(query_masks, [num_heads, 1])
+        # shape = [N*h, T_q, T_k]
+        query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, channels])
+
+        attention_probs *= query_masks
+
+        # dropout
+        attention_probs = tf.layers.dropout(attention_probs, rate=dropout_rate,
+                                            training=tf.convert_to_tensor(is_training))
+        outputs = tf.matmul(attention_probs, value_layer_)
+        # shape [batch_size, max_length, channels*num_heads]
+        outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)
+
+        # Residual connection
+        outputs += query_layer
+        # group normalization
+        outputs = group_norm(outputs)
+    return outputs
+
+
+def feed_forward(inputs, channels=None, scope="multihead_attention", activation=None, reuse=None):
+    """
+    :param inputs: [Tensor] with first dimension of "batch_size"
+    :param channels: [Int] hidden dimensions
+    :param scope: [String] name of "variable_scope"
+    :param activation: [String] name of activate function
+    :param reuse: [Boolean] tf parameter reuse
+    :return: [Tensor] outputs after feed forward with shape of "batch_size * max_length * channels"
+    """
+    if channels is None:
+        channels = [2048, 512]
+    with tf.variable_scope(scope, reuse=reuse):
+        activation_fn = get_activation(activation)
+        outputs = inputs
+        for channel in channels:
+            params = {"inputs": outputs, "num_outputs": channel, "activation_fn": activation_fn}
+            outputs = tf.contrib.layers.fully_connected(**params)
+        outputs += inputs
+        outputs = group_norm(outputs)
+    return outputs
+
+
+def label_smoothing(inputs, epsilon = 0.1):
+    """
+    :param inputs: [Tensor]
+    :param epsilon: [Float] Smoothing rate
+    :return: [Tensor] outputs after smoothing
+    """
+    return ((1 - epsilon) * inputs) + (epsilon / inputs.get_shape().as_list()[-1])
